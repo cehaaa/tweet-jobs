24db9539088be06bfa4b83ae088cba011f75881e
christianngouw <christiangouw168@gmail.com>
2021-04-12 20:25:13 +0700

    add global API endpoint

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./index.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$apiURL = "http://localhost:8000/api";
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    render: function(h) {
+        return h(App);
+    },
+}).$mount("#app");
