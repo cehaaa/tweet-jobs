6c1fe50475e7afce11b6fc6ec2c30adcda29b525
christianngouw <christiangouw168@gmail.com>
2021-04-15 00:52:03 +0700

    show data from API and fix design

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,61 +1,58 @@
 <template>
-
-	<div>
-		<div class="flex flex-col space-y-5">
-			<div class="w-full border-b p-3">
-				<div class="flex items-center space-x-5">
-					<div class="h-10 w-10 rounded-full bg-blue-500 cursor-pointer"></div>
-					<div>
-						<div class="text-gray-500"><span class="text-gray-900 font-bold">ZOO</span> @ZOO_FESS</div>
-					</div>					
-				</div>	
-				<div class="my-3 text-justify">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatibus sint dolorum nostrum maxime necessitatibus. Incidunt ab neque, ipsam ex laborum recusandae autem nam quia minima possimus, enim aperiam magnam.
-				</div>						
-			</div>
-			<div class="w-full border-b p-3">
-				<div class="flex items-center space-x-5">
-					<div class="h-10 w-10 rounded-full bg-blue-500 cursor-pointer"></div>
-					<div>
-						<div class="text-gray-500"><span class="text-gray-900 font-bold">ZOO</span> @ZOO_FESS</div>
-					</div>					
-				</div>	
-				<div class="my-3 text-justify">
-					<div class="mb-5">
-						<img src="@/assets/hack.jpg" alt="" class="rounded-lg w-full object-contain cursor-pointer ">
-					</div>
-					<div>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatibus sint dolorum nostrum maxime necessitatibus. Incidunt ab neque, ipsam ex laborum recusandae autem nam quia minima possimus, enim aperiam magnam.
-					</div>
-				</div>						
-			</div>
-			<div class="w-full border-b p-3">
-				<div class="flex items-center space-x-5">
-					<div class="h-10 w-10 rounded-full bg-blue-500 cursor-pointer"></div>
-					<div>
-						<div class="text-gray-500"><span class="text-gray-900 font-bold">ZOO</span> @ZOO_FESS</div>
-					</div>					
-				</div>	
-				<div class="my-3 text-justify">
-					<div>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatibus sint dolorum nostrum maxime necessitatibus. Incidunt ab neque, ipsam ex laborum recusandae autem nam quia minima possimus, enim aperiam magnam.
-					</div>
-					<div class="mt-3">
-						<button class="btn-primary w-32 rounded-full ">Apply</button>
-					</div>
-				</div>						
-			</div>
-		</div>
-	</div>
-
+    <div>
+        <div class="flex flex-col space-y-5">
+            <div class="w-full border-b p-3" v-for="p in post" :key="p.id">
+                <div class="flex items-center space-x-5">
+                    <div
+                        class="h-10 w-10 rounded-full bg-blue-500 cursor-pointer"
+                    ></div>
+                    <div class="group cursor-pointer">
+                        <div v-if="p.user.username" class="flex space-x-5">
+                            <div
+                                class="font-semibold group-hover:text-blue-500 duration-200"
+                            >
+                                {{ p.user.username.split(" ").pop() }}
+                            </div>
+                            <div>@{{ p.user.username }}</div>
+                        </div>
+                        <div v-else class="flex space-x-5">
+                            <div
+                                class="font-semibold group-hover:text-blue-500 duration-200"
+                            >
+                                Unkown Username
+                            </div>
+                            <div>@username</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="my-3 text-justify">
+                    <div class="mb-5" v-if="p.picture != null">
+                        <div class="overflow-hidden rounded-lg">
+                            <img
+                                :src="'http://localhost:8000/post/' + p.picture"
+                                alt=""
+                                class="rounded-lg w-full object-contain cursor-pointer transform duration-300 hover:scale-110 "
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        {{ p.desc }}
+                    </div>
+                </div>
+                <div class="mt-2" v-if="p.status == 'job'">
+                    <button class="btn-primary w-32 rounded-full ">
+                        Apply
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
-}
+    props: ["post"],
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
