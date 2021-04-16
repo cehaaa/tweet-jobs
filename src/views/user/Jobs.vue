a623597025562a0b214805ae75c235d13083f045
christianngouw <christiangouw168@gmail.com>
2021-04-16 10:52:30 +0700

    load jobs data

---
+++ src/views/user/Jobs.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="p-3 border-b flex justify-between w-full">
+            <div class="font-bold text-2xl text-blue-500">Jobs</div>
+        </div>
+        <div class="w-full">
+            <tweetJobs-post :post="post"></tweetJobs-post>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            post: "",
+        };
+    },
+    components: {
+        "tweetJobs-post": () => import("@/components/Post"),
+    },
+    mounted() {
+        this.getPost();
+    },
+    methods: {
+        getPost() {
+            fetch(this.$apiURL + "/jobs", {
+                method: "get",
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    this.post = data;
+                });
+        },
+    },
+};
+</script>
+
+<style></style>
