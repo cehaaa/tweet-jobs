cd458b917f64b58799841d894bbf521c6181b0fc
christianngouw <christiangouw168@gmail.com>
2021-04-17 21:05:02 +0700

    code refactoring

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -1,100 +1,114 @@
 <template>
-    <div class="p-3">
-        <div class="flex flex-col space-y-5 text-gray-500">
-            <div class="flex space-x-3">
-                <div class="w-6/12">
-                    <div>Username</div>
-                    <input
-                        type="text"
-                        class="form-control"
-                        v-model="userData.userename"
-                    />
+    <div>
+        <div class="px-5 py-3 border-b flex space-x-5 items-center w-full">
+            <router-link to="home">
+                <img src="@/assets/left-arrow.png" alt="" class="w-8" />
+            </router-link>
+            <div>
+                <div
+                    class="font-semibold text-base"
+                    v-if="userProfile.username"
+                >
+                    {{ userProfile.username }}
                 </div>
-                <div class="w-6/12">
-                    <div>Email</div>
-                    <input
-                        type="email"
-                        class="form-control"
-                        v-model="userData.email"
-                    />
+                <div v-else>
+                    Username
                 </div>
-            </div>
-            <div class="flex space-x-3">
-                <div class="w-6/12">
-                    <div>Entry year</div>
-                    <input
-                        type="number"
-                        class="form-control"
-                        v-model="userData.entry_year"
-                    />
+                <div class="text-gray-500 text-xs">
+                    {{ sumOfPost || 0 }} Tweets
                 </div>
-                <div class="w-6/12">
-                    <div>Graduation year</div>
-                    <input
-                        type="number"
-                        class="form-control"
-                        v-model="userData.graduation_year"
-                    />
-                </div>
-            </div>
-            <div class="flex space-x-3">
-                <div class="w-6/12">
-                    <div>Major</div>
-                    <input
-                        type="text"
-                        class="form-control"
-                        v-model="userData.major"
-                    />
-                </div>
-                <div class="w-6/12">
-                    <div class="text-gray-500">Date of birth</div>
-                    <input
-                        type="date"
-                        class="form-control"
-                        v-model="userData.date_of_birth"
-                    />
-                </div>
-            </div>
-            <div class="flex space-x-3">
-                <div class="w-6/12">
-                    <div>Job</div>
-                    <input
-                        type="text"
-                        class="form-control"
-                        v-model="userData.job"
-                    />
-                </div>
-
-                <div class="w-6/12">
-                    <div>Phone number</div>
-                    <input
-                        type="number"
-                        class="form-control"
-                        v-model="userData.phone_number"
-                    />
-                </div>
-            </div>
-            <div class="">
-                <div>Password</div>
-                <input
-                    type="password"
-                    class="form-control"
-                    v-model="userData.password"
-                />
-            </div>
-            <div class="">
-                <div>Address</div>
-                <textarea
-                    cols="50"
-                    rows="5"
-                    class="form-control"
-                    v-model="userData.address"
-                ></textarea>
             </div>
         </div>
-
-        <div class="mt-5 flex">
-            <button class="btn-primary ml-auto w-32 rounded-full">Save</button>
+        <div>
+            <div class="bg-blue-500 h-40"></div>
+            <div class="-mt-14 px-5">
+                <img
+                    v-if="userProfile.profile_img"
+                    :src="
+                        'http://localhost:8000/profile/' +
+                            userProfile.profile_img
+                    "
+                    class="h-32 w-32 border-2 rounded-full object-cover"
+                />
+                <div
+                    v-else
+                    class="h-32 w-32 border-2 rounded-full bg-gray-500"
+                ></div>
+                <div class="flex justify-between">
+                    <div v-if="userProfile.username">
+                        <div class="text-2xl font-semibold mt-1">
+                            {{ userProfile.username }}
+                        </div>
+                        <div class="text-gray-500 lowercase">
+                            @{{ userProfile.username }}
+                        </div>
+                    </div>
+                    <div v-else>
+                        <div class="text-2xl font-semibold mt-1">
+                            Username
+                        </div>
+                        <div class="text-gray-500 lowercase">
+                            @Username
+                        </div>
+                    </div>
+                    <div>
+                        <router-link
+                            to="/tweet/settings"
+                            class="btn-outline-primary rounded-full"
+                        >
+                            Edit profile
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="px-5 py-2 border-b pb-10">
+            <div v-if="userProfile.job">Work as {{ userProfile.job }} 💖</div>
+            <div v-else>update your profile</div>
+        </div>
+        <div class="px-5 pt-5">
+            <div class="flex flex-col space-y-5">
+                <div
+                    v-for="post in userProfile.post"
+                    :key="post.id"
+                    class="border-b"
+                >
+                    <div class="flex space-x-5 pb-5">
+                        <img
+                            :src="
+                                'http://localhost:8000/profile/' +
+                                    userProfile.profile_img
+                            "
+                            class="h-12 w-12 rounded-full object-cover"
+                            alt=""
+                        />
+                        <div>
+                            <div class="font-semibold">
+                                {{ userProfile.username }}
+                            </div>
+                            <div class="text-gray-500 text-sm">
+                                @{{ userProfile.username }}
+                            </div>
+                            <div class="mt-2 mb-5">{{ post.desc }}</div>
+                            <div class="overflow-hidden rounded-lg">
+                                <img
+                                    :src="
+                                        'http://localhost:8000/post/' +
+                                            post.picture
+                                    "
+                                    alt=""
+                                    class="rounded-lg w-full  object-cover cursor-pointer transform duration-300 hover:scale-110"
+                                />
+                            </div>
+                            <div v-if="post.status == 'job'" class="mt-5">
+                                <button class="btn-primary rounded-full w-32">
+                                    Apply
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -103,36 +117,34 @@
 export default {
     data() {
         return {
-            userData: {
-                username: "",
-                email: "",
-                password: "",
-                entry_year: "",
-                graduation_year: "",
-                major: "",
-                date_of_birth: "",
-                address: "",
-                phone_number: "",
-                job: "",
-            },
+            userProfile: "",
+            sumOfPost: "",
+            isAdd: false,
+            userDesc: "",
         };
     },
     mounted() {
-        this.getUserData();
+        this.getUserProfile();
     },
     methods: {
-        getUserData() {
-            let userId = JSON.parse(localStorage.getItem("TweetJobs_UserId"));
-
-            fetch(`${this.$apiURL}/user_information/${userId}`, {
+        getUserProfile() {
+            fetch(`${this.$apiURL}/user/post/${this.$userId}`, {
                 method: "get",
             })
                 .then((res) => res.json())
                 .then((data) => {
-                    for (let key in this.userData) {
-                        this.userData[key] = data[key];
-                    }
+                    this.userProfile = data;
+
+                    this.sumOfPost = data.post.length;
                 });
+        },
+        storeAboutMe() {
+            localStorage.setItem(
+                "TweetJobs_User_AboutMe",
+                JSON.stringify(this.userDesc)
+            );
+
+            this.isAdd = !this.isAdd;
         },
     },
 };
