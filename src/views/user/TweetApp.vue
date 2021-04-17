f707a87fcd53a21b6a36d9157f97c6a76e6fa9b6
christianngouw <christiangouw168@gmail.com>
2021-04-17 21:05:42 +0700

    fix design and code refactoring

--- src/views/user/TweetApp.vue
+++ src/views/user/TweetApp.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full flex h-screen">
-        <div class="w-3/12 border-r-2 pt-5 flex flex-col">
+        <div class="w-3/12 border-r pt-5 flex flex-col">
             <div
                 class="text-3xl font-bold px-10 hover:text-blue-500 duration-200 cursor-pointer"
             >
@@ -11,9 +11,7 @@
                 <div
                     class="mt-10 text-lg font-semibold flex-row space-y-10 px-10"
                 >
-                    <router-link
-                        class="sidebar-list text-blue-500"
-                        to="/tweet/home"
+                    <router-link class="sidebar-list" to="/tweet/home"
                         >Home</router-link
                     >
 
@@ -21,8 +19,8 @@
                         >Jobs</router-link
                     >
 
-                    <router-link class="sidebar-list" to="/tweet/home"
-                        >Save</router-link
+                    <router-link class="sidebar-list" to="/tweet/settings"
+                        >Settings</router-link
                     >
                 </div>
 
@@ -31,19 +29,35 @@
                         class="hover:bg-gray-200 duration-200 cursor-pointer group px-10 py-5"
                     >
                         <div class="flex items-center space-x-2">
+                            <img
+                                :src="
+                                    'http://localhost:8000/profile/' +
+                                        userData.profile
+                                "
+                                v-if="userData.profile"
+                                class="rounded-full h-10 w-10 object-cover"
+                            />
                             <div
-                                class="rounded-full h-10 w-10 bg-blue-500"
+                                v-else
+                                class="rounded-full h-10 w-10 bg-gray-500 "
                             ></div>
-                            <div>
+                            <div v-if="userData.username && userData.job">
                                 <div
                                     class="font-semibold group-hover:underline"
                                 >
                                     {{ userData.username }}
                                 </div>
                                 <div
-                                    class="text-gray-500 group-hover:underline"
+                                    class="text-gray-500 group-hover:underline text-sm"
                                 >
                                     {{ userData.job }}
+                                </div>
+                            </div>
+                            <div v-else>
+                                <div
+                                    class="text-gray-500 group-hover:underline text-sm"
+                                >
+                                    Update profile.
                                 </div>
                             </div>
                         </div>
@@ -52,11 +66,17 @@
             </div>
         </div>
 
-        <div class="w-6/12 border-r-2 h-screen overflow-auto hide-scroll-bar">
+        <div class="w-8/12 border-r h-screen overflow-auto hide-scroll-bar">
             <router-view></router-view>
         </div>
 
-        <div class="w-3/12"></div>
+        <div class="w-5/12">
+            <div class="border-b p-3 w-full">
+                <div class="font-bold text-2xl">You might like</div>
+
+                <div class="flex flex-col space-y-3"></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -65,8 +85,9 @@ export default {
     data() {
         return {
             userData: {
-                username: "Username",
-                job: "Your Dream Job",
+                username: "",
+                job: "",
+                profile: "",
             },
         };
     },
@@ -75,9 +96,7 @@ export default {
     },
     methods: {
         getUserData() {
-            let userId = JSON.parse(localStorage.getItem("TweetJobs_UserId"));
-
-            fetch(`${this.$apiURL}/user_information/${userId}`, {
+            fetch(`${this.$apiURL}/user/detail/${this.$userId}`, {
                 method: "get",
             })
                 .then((res) => res.json())
@@ -85,6 +104,7 @@ export default {
                     if (data.username && data.job !== null) {
                         this.userData.username = data.username;
                         this.userData.job = data.job;
+                        this.userData.profile = data.profile_img;
                     }
                 });
         },
