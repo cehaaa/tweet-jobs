b90d6db29542fe5465017fcd216fc9e1cc026d00
christianngouw <christiangouw168@gmail.com>
2021-04-17 21:04:27 +0700

    code refactoring

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -2,12 +2,24 @@
     <div>
         <div class="flex flex-col space-y-5">
             <div class="w-full border-b p-3" v-for="p in post" :key="p.id">
-                <div class="flex items-center space-x-5">
+                <div class="flex space-x-5">
+                    <img
+                        :src="
+                            'http://localhost:8000/profile/' +
+                                p.user.profile_img
+                        "
+                        v-if="p.user.profile_img"
+                        class="rounded-full object-cover h-12 w-12"
+                    />
                     <div
-                        class="h-10 w-10 rounded-full bg-blue-500 cursor-pointer"
+                        v-else
+                        class="h-12 w-12 bg-gray-500 rounded-full"
                     ></div>
-                    <div class="group cursor-pointer">
-                        <div v-if="p.user.username" class="flex space-x-5">
+                    <div>
+                        <div
+                            v-if="p.user.username"
+                            class="flex space-x-2 group cursor-pointer"
+                        >
                             <div
                                 class="font-semibold group-hover:text-blue-500 duration-200"
                             >
@@ -15,34 +27,27 @@
                             </div>
                             <div>@{{ p.user.username }}</div>
                         </div>
-                        <div v-else class="flex space-x-5">
-                            <div
-                                class="font-semibold group-hover:text-blue-500 duration-200"
-                            >
-                                Unkown Username
+                        <div class="mt-2">
+                            <div>{{ p.desc }}</div>
+                            <div v-if="p.picture != null" class="mt-5">
+                                <div class="overflow-hidden rounded-lg">
+                                    <img
+                                        :src="
+                                            'http://localhost:8000/post/' +
+                                                p.picture
+                                        "
+                                        alt=""
+                                        class="rounded-lg w-full  object-cover cursor-pointer transform duration-300 hover:scale-110"
+                                    />
+                                </div>
                             </div>
-                            <div>@username</div>
+                            <div class="mt-5" v-if="p.status == 'job'">
+                                <button class="btn-primary w-32 rounded-full ">
+                                    Apply
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="my-3 text-justify">
-                    <div class="mb-5" v-if="p.picture != null">
-                        <div class="overflow-hidden rounded-lg">
-                            <img
-                                :src="'http://localhost:8000/post/' + p.picture"
-                                alt=""
-                                class="rounded-lg w-full object-contain cursor-pointer transform duration-300 hover:scale-110 "
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        {{ p.desc }}
-                    </div>
-                </div>
-                <div class="mt-2" v-if="p.status == 'job'">
-                    <button class="btn-primary w-32 rounded-full ">
-                        Apply
-                    </button>
                 </div>
             </div>
         </div>
